61bd4ddb2296118c764ea697bc96afd03ecd232a
Augusto Lopes Maia <augusto_hl@hotmail.com>
2026-09-12 15:43:50 -0300

    Criando Filtro Card / Tarefas / Models / Store e o Reducer.

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eee;
+  height: 100vh;
+`
+
+export const Filtro = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+  margin-top: 16px;
+`
+export const Campo = styled.input`
+  padding: 8px;
+  backgrund-color: #fff;
+  border-radius: 8px;
+  font-weight: bold;
+  color: #666666;
+  border-color: #666666;
+  max-width: 100%;
+`
---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux'
+
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import { RootReducer } from '../../store'
+
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+  return (
+    <Container>
+      <p>
+        2 Tarefas marcadas como: &quot;Categoria&ldquo; e &quot;Termo&ldquo;
+      </p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+
+export default ListaDeTarefas
