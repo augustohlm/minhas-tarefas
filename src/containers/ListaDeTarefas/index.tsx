69b9b9222eaff34c7335e59d083d46b2dd973e47
Augusto Lopes Maia <augusto_hl@hotmail.com>
2026-09-14 15:10:25 -0300

    Ajustando e efetivando a remoção de tarefas.

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -5,16 +5,17 @@ import { Container } from './styles'
 import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
   return (
     <Container>
       <p>
         2 Tarefas marcadas como: &quot;Categoria&ldquo; e &quot;Termo&ldquo;
       </p>
       <ul>
-        {tarefas.map((t) => (
+        {itens.map((t) => (
           <li key={t.titulo}>
             <Tarefa
+              id={t.id}
               descricao={t.descricao}
               titulo={t.titulo}
               status={t.status}
